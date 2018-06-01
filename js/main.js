0aca29b904fd512e3a3c12afcb93d6419aff9794
Unknown <poul.apple@gmail.com>
2018-06-01 17:41:36 +0300

    10-about

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        nextArrow: '<button type="button" class="slick-next"><img src="img/r-arrow.png" alt="Далее"></button>',
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/l-arrow.png" alt="Назад"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="img/r-arrow-a.png" alt="Далее"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/l-arrow-a.png" alt="Назад"></button>'
     });
     /* ---Slider end--- */
 
@@ -95,72 +95,120 @@ $(document).ready(function(){
     // }
 
 
-    ymaps.ready(function () {
-        var myMap = new ymaps.Map('map', {
-                center: [59.129558, 37.918355],
-                zoom: 17,
-                controls: []
-            }, {
-                searchControlProvider: 'yandex#search'
-            });
+    // ymaps.ready(function () {
+     //    var myMap = new ymaps.Map('map', {
+     //            center: [59.129558, 37.918355],
+     //            zoom: 17,
+     //            controls: []
+     //        }, {
+     //            searchControlProvider: 'yandex#search'
+     //        });
+    //
+     //    var myMap2 = new ymaps.Map('map2', {
+     //        center: [59.129558, 37.918355],
+     //        zoom: 17,
+     //        controls: []
+     //    }, {
+     //        searchControlProvider: 'yandex#search'
+     //    });
+    //
+    //
+     //        // Создаём макет содержимого.
+     //        // MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+     //        //     '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+     //        // );
+    //
+     //        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+     //            hintContent: 'ул. К.Маркса, д. 78',
+     //            balloonContent: 'ул. К.Маркса, д. 78',
+     //            iconCaption: 'ул. К.Маркса, д. 78'
+     //        }, {
+     //            // Опции.
+     //            // Необходимо указать данный тип макета.
+     //            iconLayout: 'default#image',
+     //            // Своё изображение иконки метки.
+     //            iconImageHref: 'img/mark.png',
+     //            // Размеры метки.
+     //            iconImageSize: [44, 60],
+     //            // Смещение левого верхнего угла иконки относительно
+     //            // её "ножки" (точки привязки).
+     //            iconImageOffset: [-20, -60],
+     //        });
+    //
+     //    myPlacemark2 = new ymaps.Placemark(myMap2.getCenter(), {
+     //        hintContent: 'ул. К.Маркса, д. 78',
+     //        balloonContent: 'ул. К.Маркса, д. 78',
+     //        iconCaption: 'ул. К.Маркса, д. 78'
+     //    }, {
+     //        // Опции.
+     //        // Необходимо указать данный тип макета.
+     //        // iconLayout: 'default#image',
+     //        // Своё изображение иконки метки.
+     //        iconImageHref: 'img/mark.png',
+     //        // Размеры метки.
+     //        iconImageSize: [44, 60],
+     //        // Смещение левого верхнего угла иконки относительно
+     //        // её "ножки" (точки привязки).
+     //        iconImageOffset: [-20, -60],
+     //        preset: 'islands#dotIcon',
+     //        iconColor: '#ab1010'
+     //    })
+    //
+     //    	myMap.geoObjects.add(myPlacemark);
+     //   		myMap.behaviors.disable('scrollZoom')
+    //
+	// 		myMap2.geoObjects.add(myPlacemark2);
+	// 		myMap2.behaviors.disable('scrollZoom')
+     //        // .add(myPlacemarkWithContent);
+	// });
 
-        var myMap2 = new ymaps.Map('map2', {
-            center: [59.129558, 37.918355],
-            zoom: 17,
-            controls: []
-        }, {
-            searchControlProvider: 'yandex#search'
+
+
+
+//youtube script
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    var player;
+
+    onYouTubeIframeAPIReady = function () {
+        player = new YT.Player('player', {
+            height: '370px',
+            width: '100%',
+            videoId: 'yGlzI3LXQ8E',  // youtube video id
+            playerVars: {
+                'autoplay': 0,
+                'rel': 0,
+                'showinfo': 0
+            },
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
         });
+    }
+
+    var p = document.getElementById ("player");
+    $(p).hide();
+
+    // var t = document.getElementById ("thumbnail");
+    // t.src = "http://img.youtube.com/vi/AkyQgpqRyBY/0.jpg";
+
+    onPlayerStateChange = function (event) {
+        if (event.data == YT.PlayerState.ENDED) {
+            $('.start-video').fadeIn('normal');
+        }
+    }
+
+    $(document).on('click', '.start-video', function (e) {
+        $(this).hide();
+        $("#player").show();
+        $("#thumbnail_container").hide();
+        player.playVideo();
+    });
 
 
-            // Создаём макет содержимого.
-            // MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-            //     '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-            // );
-
-            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-                hintContent: 'ул. К.Маркса, д. 78',
-                balloonContent: 'ул. К.Маркса, д. 78',
-                iconCaption: 'ул. К.Маркса, д. 78'
-            }, {
-                // Опции.
-                // Необходимо указать данный тип макета.
-                iconLayout: 'default#image',
-                // Своё изображение иконки метки.
-                iconImageHref: 'img/mark.png',
-                // Размеры метки.
-                iconImageSize: [44, 60],
-                // Смещение левого верхнего угла иконки относительно
-                // её "ножки" (точки привязки).
-                iconImageOffset: [-20, -60],
-            });
-
-        myPlacemark2 = new ymaps.Placemark(myMap2.getCenter(), {
-            hintContent: 'ул. К.Маркса, д. 78',
-            balloonContent: 'ул. К.Маркса, д. 78',
-            iconCaption: 'ул. К.Маркса, д. 78'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            // iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: 'img/mark.png',
-            // Размеры метки.
-            iconImageSize: [44, 60],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-20, -60],
-            preset: 'islands#dotIcon',
-            iconColor: '#ab1010'
-        })
-
-        	myMap.geoObjects.add(myPlacemark);
-       		myMap.behaviors.disable('scrollZoom')
-
-			myMap2.geoObjects.add(myPlacemark2);
-			myMap2.behaviors.disable('scrollZoom')
-            // .add(myPlacemarkWithContent);
-	});
 
 
 });
