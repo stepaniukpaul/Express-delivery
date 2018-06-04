16b645351bc912d22904d4b60309bf0b26f8f034
Unknown <poul.apple@gmail.com>
2018-06-04 17:43:20 +0300

    PopUp (Личный каб)

--- js/main.js
+++ js/main.js
@@ -188,8 +188,37 @@ $(document).ready(function(){
         $("#thumbnail_container").hide();
         player.playVideo();
     });
+//youtube script end
 
+//Маска ввода
+    $(function(){
+        $("#phone").mask("+7(999) 999-99-99");
+    });
 
+// PopUp
+    $(document).ready(function() {
+        $('.popup-with-form').magnificPopup({
+            type: 'inline',
+            preloader: false,
+            focus: '#name',
 
+            // When elemened is focused, some mobile browsers in some cases zoom in
+            // It looks not nice, so we disable it:
+            callbacks: {
+                beforeOpen: function() {
+                    if($(window).width() < 700) {
+                        this.st.focus = false;
+                    } else {
+                        this.st.focus = '#name';
+                    }
+                }
+            }
+        });
+        $(document).on('click', '.popup-modal-dismiss', function (e) {
+            e.preventDefault();
+            $.magnificPopup.close();
+        });
+    });
+// PopUp end
 
 });
