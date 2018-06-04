4c5cf44adfd80426edfede3acd6a6e3b62dd7e3c
Unknown <poul.apple@gmail.com>
2018-06-04 18:46:11 +0300

    PopUp (рег)

--- js/main.js
+++ js/main.js
@@ -192,7 +192,7 @@ $(document).ready(function(){
 
 //Маска ввода
     $(function(){
-        $("#phone").mask("+7(999) 999-99-99");
+        $(".phone-mask").mask("+7(999) 999-99-99");
     });
 
 // PopUp
